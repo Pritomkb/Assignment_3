2e71ed3dd19813f0720ba541dee795df43a9b0df
Pritom kb <pritombiswas0188@gmail.com>
2025-01-31 02:03:13 +0600

    header commit

--- JS/tailwind.config.js
+++ JS/tailwind.config.js
@@ -6,3 +6,13 @@ module.exports = {
     },
     plugins: [],
   }
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
